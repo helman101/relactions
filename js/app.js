6168e25527a63beaee320af1fda427a88fdb2dd2
helman101 <helmanandresortegon@gmail.com>
2024-02-07 22:14:18 +0100

    FEAT: initialize and configure foundation & splide

---
+++ js/app.js
@@ -0,0 +1,14 @@
+$(document).foundation()
+
+new Splide( '.splide', {
+  type: "loop",
+  perPage: 1,
+  autoplay: true,
+  arrows: false,
+  pagination: false,
+  easing: 'ease',
+  speed: 1000
+}).mount();
+
+
+
